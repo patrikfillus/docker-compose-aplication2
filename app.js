8228be134dddafc93b4af119161ee713cca3988e
Patrik Fillus <patrik.fillus@live.com>
2019-11-12 00:19:00 -0300

    firt commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var os = require('os');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`Hostname: ${os.hostname()}\n
+    {
+      "telefone": "3376-1921",
+      "DDD": "41",
+      "celular": "995897781",
+      "email": "email@gmail.com",
+    }
+    `);
+}).listen(8080);
